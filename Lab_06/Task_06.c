be925088571dcff4248104b85af904c9bc8a95d7
Syed Najam U Saqib <136888817+syednajamusaqib@users.noreply.github.com>
2023-12-03 22:08:37 +0500

    Update Task_06.c

--- Lab_06/Task_06.c
+++ Lab_06/Task_06.c
@@ -1,115 +1,53 @@
-#include <stdio.h>
-#include <stdbool.h>
-#define SIZE 100
-
-
-void toUpper(char str[SIZE]){
-    int i = 0;
-    while(str[i] != '\0'){
-        if(str[i] >= 'a' || str[i] <='z'){
-            str[i] = str[i] - 32;
-        }
-        i++;
-    }
-    // str[i] = '\0';
-    printf("%s", str);
-    // return str;
-}
-
-
-// void toLower(char str[SIZE]){
-//     int i = 0;
-//     char new[SIZE] = toUpper(str);
-//     while(new[i] != '\0'){
-//         if(new[i] >= 'a' || new[i] <='z'){
-//             new[i] = new[i] - 32;
-//         }
-//         i++;
-//     }
-//     // str[i] = '\0';
-//     printf("%s", new);
-// }
-
-void string(char str[]){
-    // int i = 0;
-    // while(str[i] != '\0'){
-        for(int i = 0;i<SIZE; i++){
-        if((str[i] >= 'A' && str[i] <='Z') || (str[i] >= 'a' && str[i] <='z')){
-        if(str[i] >= 'A' && str[i] <='Z'){
-            str[i] = str[i] + 32;
-            // printf("%c", str[i]);
-        }
-        if(str[i] >= 'a' && str[i] <='z'){
-            str[i] = str[i] - 32;
-            // printf("%c", str[i]);
-        }
+// convert all uppercase letters to lowercase and viceversa in a given string(char array)
+#include<stdio.h>
+int finding_length(char string[]){
+    int length = 0;
+    while(string[length]!='\0'){
+        length++;
         
     }
-        // i++;
-        }
-    printf("%s", str);
-    // }
-    // str[i] = '\0';
+    return length;
 }
-
-// char isaplha(char str[],char newstr[]){
-//     int a = 1;
-//     if((str[i] >= 'A' || str[i] <='Z') && a==1){
-//         str[i] = str[i] 
-//     }
-// }
-
-
-int numberValidation(int number) {
-    bool validInput = false;
-    while (!validInput) {
-        if (scanf("%d", &number) == 1) {
-            if(number < 1 || number > 2){
-                puts("Please Enter Valid Choice: ");
-            }else{
-                validInput = true;
-            }
-        } else {
-            printf("Invalid input. Please enter a valid number.\n");
-            while (getchar() != '\n');
+void convert_lowercase(char array[],int length)
+{
+    for(int i=0;i<length;i++){
+        if(array[i]>='A' && array[i]<='Z'){
+            array[i] = array[i] + 32;
         }
     }
-    return number;
+
+}
+void uppercase(char array[],int length){
+    for(int i=0;i<length;i++){
+        if(array[i]>='a' && array[i]<='z'){
+            array[i] = array[i] - 32;
+        }
+    }
+    
 }
 
-int main(void) {
-    char str[SIZE];
 
-    puts("Enter String: ");
-
-    scanf("%[^\n]",str);
-
-    // puts("1 for Upper to Lower");
-
-    // int choice = numberValidation(choice);
-
-    // fgets(str, SIZE,stdin);
-
-    // if(choice == 1){
-    //     toLower(str);
-    // }
-    // else{
-        // toUpper(str);
-    // }
-    string(str);
-
-    // int i = 0;
-    // char new[SIZE] = toUpper(str);
-    // while(new[i] != '\0'){
-    //     if(new[i] >= 'a' || new[i] <='z'){
-    //         new[i] = new[i] - 32;
-    //     }
-    //     i++;
-    // }
-    // // str[i] = '\0';
-    // printf("%s", new);
+int main()
+{
+    int min=200;
+    char array[min];
+    printf("Enter the string: \n");
+    gets(array);
 
 
 
-    return 0;
+    
+    int length = finding_length(array);
+    printf("Converting lowercase: ");
+    convert_lowercase(array,length);
+    printf("%s \n",array);
+    printf("Converting upper case: ");
+    uppercase(array,length);
+    printf("%s",array);
+
+
+
+
+
+    return 0;
 }
